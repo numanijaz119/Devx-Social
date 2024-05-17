ebce579c2b27eaab7c258570dde090ffb9d97e5d
Muhammad Numan Ijaz <minibhai009@gmail.com>
2024-05-17 18:40:04 +0500

    bugfix env

--- devx/settings.py
+++ devx/settings.py
@@ -93,11 +93,12 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'PASSWORD': os.environ.get('DB_PASS'),
         'HOST': os.environ.get('DB_HOST'),
         'PORT': '5432',
     }
 }
+
 
 
 
